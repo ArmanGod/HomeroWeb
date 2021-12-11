cb19d1a4a61f1c89a9b04c73a7c043f786693cb5
ArmandoGod <armanpino01@hotmail.com>
2021-12-11 02:21:02 -0300

    Update

--- HomeroWeb/settings.py
+++ HomeroWeb/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'soportehomeroapps@gmail.com'
+EMAIL_HOST_PASSWORD = 'Homeroapli'
+EMAIL_USE_TLS = True
